fb75de559be32f8e85397557321a5f60988ddbb7
Keysie27 <keysiesanchezrosario270301@gmail.com>
2025-07-14 01:06:22 -0400

    refactor: remove tree model

--- models/sample_models/decision_tree.py
+++
@@ -1,6 +0,0 @@
-from sklearn.tree import DecisionTreeClassifier
-
-def train(X, y, max_depth=None, **kwargs):
-    model = DecisionTreeClassifier(max_depth=max_depth, **kwargs)
-    model.fit(X, y)
-    return model
